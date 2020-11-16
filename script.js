9c224f9140188f66a7cb4d7bb14580c581595b07
Swaroop D <swaroopshri2019@gmail.com>
2020-11-17 00:22:39 +0530

    digital time functionality

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-(function () {
-  const time_container = document.getElementById("event-time");
+(function (target) {
+  function time_output() {
+    let date_handler = new Date(),
+      hr_handler = date_handler.getHours(),
+      min_handler = date_handler.getMinutes();
+    hr_handler = hr_handler % 12 || 12;
+    let output_string = `${hr_handler}<span>:</span>${equal_val(min_handler)}`;
 
-  const time_handler = () => {
-    const date_handler = new Date(),
-      hour_handler = date_handler.getHours(),
-      min_handler = date_handler.getMinutes(),
-      output_handler = `${hour_handler}<span>:</span>${min_handler}`;
+    return output_string;
+  }
+  function equal_val(notation) {
+    return (parseInt(notation, 10) < 10 ? '0' : '') + notation;
+  }
 
-    return output_handler;
-  };
-
-  time_container.innerHTML = time_handler();
-})();
+  function time_handler() {
+    target.innerHTML = time_output();
+    setTimeout(time_handler, 1000);
+  } time_handler()
+})(document.querySelector("#event-time"));
